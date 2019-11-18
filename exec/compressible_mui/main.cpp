d90f776f8bae3f28432e3c9f6b887be321b13a20
Changho Kim <ckim103@ucmerced.edu>
2019-11-18 15:50:09 -0800

    main.cpp: mui::mpi_split_by_app added GNUmakefile: mui directory included

--- exec/compressible_mui/main.cpp
+++ exec/compressible_mui/main.cpp
@@ -1,6 +1,11 @@
 #include <AMReX.H>
 #include <iostream>
 
+// additional header files needed by MUI
+#include <mpi.h>
+#include <lib_mpi_split.h>
+#include <mui.h>
+
 // function declaration
 void main_driver (const char* argv);
 
@@ -10,7 +15,9 @@ int main (int argc, char* argv[])
 {
     cout << "Hello World from compressible_mui\n";
 
-    amrex::Initialize(argc,argv);
+    MPI_Comm comm = mui::mpi_split_by_app( argc, argv );
+    amrex::Initialize(argc,argv,true,comm);
+    //amrex::Initialize(argc,argv);
 
     // argv[1] contains the name of the inputs file entered at the command line
     main_driver(argv[1]);
